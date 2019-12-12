44bd0be36cf302ffdcc51a3b484e0b9b5b3c971c
kwz <1912597606@qq.com>
2019-12-12 13:28:44 +0800

    备份到dev

--- src/main/java/com/sean/backend/service/TestService.java
+++ src/main/java/com/sean/backend/service/TestService.java
@@ -3,6 +3,7 @@ package com.sean.backend.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sean.backend.dao.TestDao;
+import com.sean.backend.entity.courseModule.seleCourse;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
@@ -11,9 +12,10 @@ public class TestService {
     @Resource
     private TestDao testDao;
 
-    public String StudentFindOne() throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        String json = mapper.writeValueAsString(testDao.findById(1));
-        return json;
+
+    public String StudentFindOne()  {
+
+
+        return testDao.findById(1).toString();
     }
 }
